1dbdbcef9579ddd30da1927e3c6d8466168bad49
lukaas <sakula.hys@polytechnique.org>
2025-11-18 13:39:42 +0100

    C++ - 20251118 - tutorial chapter 4 Q3

--- HelloWorld_C++/main.cpp
+++ HelloWorld_C++/main.cpp
@@ -2,14 +2,35 @@
 #include "add.h"
 #include "io.h"
 
+double heightAtTime(int time){
+
+    double g{9.8};
+    double distance = g * time*time / 2.;
+    return distance;
+}
+
+int readHeight(){
+    int x{};
+    std::cout << "Enter the height of the tower in meters: " << '\n';
+    std::cin >> x;
+    return x;
+}
 
 
 int main(){
 
-    double x {readDouble()};
-    double y {readDouble()};
 
-    readOperation(x,y);
+    int height{readHeight()};
+
+    for (int i=0;i < 6;i++){
+        double h{heightAtTime(i) };
+        if (height - h  > 0){
+            std::cout << "At " << i << " seconds, the ball is at height: " << height - h << " meters\n";
+        }
+        else{
+            std::cout << "At " << i << " seconds, the ball is on the ground.\n";
+        }
+    }
 
     return 0;
 }
